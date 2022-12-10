d1787d805e8d01e67db966bf57094549a77bce53
Simon Y <therealkjai139@gmail.com>
2022-12-09 19:17:42 -0500

    tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [newHeading, setNewHeading] = useState('Old heading')
+
+  const useNewHeading = () => {
+    setNewHeading('New heading')
+  }
+
   return (
     <div className="App">
       <h1>Hello from App</h1>
+      <p className='newTxt'>{newHeading}</p>
+      <button onClick={useNewHeading}>Use new Heading</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import { Profile } from './Profile';
 
-test('renders learn react link', () => {
+test('renders homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloApp = screen.getByText(/Hello from App/i);
+  expect(helloApp).toBeInTheDocument();
 });
+
+
+test("Profile component", () => {
+  
+    render(<Profile />);
+    const helloProfile = screen.getByText(/Hello from Profile/i)
+    expect(helloProfile).toBeInTheDocument()
+  
+}) 
+
+test("App render", () => {
+  const { container } = render(<App />)
+  expect(container).toMatchSnapshot()
+})
+
+
+test('new heading after click', () => {
+  render(<App />)
+
+  const button = screen.getByRole("button", {name: "Use new Heading" })
+
+  userEvent.click(button)
+
+  expect(screen.getByRole("button").textContent).toMatch(/New heading/i)
+})
